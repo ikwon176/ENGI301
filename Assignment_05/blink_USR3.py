b57f4f0bf417211ebba4e40d0300ce251d7823e0
ikwon176 <ikk1@rice.edu>
2019-10-08 13:58:11 -0500

    Working code for 5 Hz blinking of USR3 LED

--- Assignment_05/blink_USR3.py
+++ Assignment_05/blink_USR3.py
@@ -1,13 +1,59 @@
+"""
+--------------------------------------------------------------------------
+Blink USR3 LED
+--------------------------------------------------------------------------
+Credits:
+The BeagleBone IO Python library was originally forked from the excellent 
+MIT Licensed RPi.GPIO library written by Ben Croston.
+
+Basic structure of code (Adafruit's BeagleBone IO Python Library) written 
+by Justin Cooper, Adafruit Industries. BeagleBone IO Python library is 
+released under the MIT License.
+
+License:   
+Copyright 2019 Irene Kwon
+
+Redistribution and use in source and binary forms, with or without 
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, 
+this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, 
+this list of conditions and the following disclaimer in the documentation 
+and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors 
+may be used to endorse or promote products derived from this software without 
+specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+THE POSSIBILITY OF SUCH DAMAGE.
+--------------------------------------------------------------------------
+Description:
+    - Want USR3 LED on pocketbeagle to flash at a frequency of 5 Hz
+        - want 5 Hz and time.sleep(in seconds) so we want 1/5 for the whole
+          cycle or 1/10 seconds for each part (on) and then 1/10 seconds off
+--------------------------------------------------------------------------
+"""
 import Adafruit_BBIO.GPIO as GPIO
 import time
 
-for i in range(4):
-    GPIO.setup("USR%d" % i, GPIO.OUT)
+i = 3
+GPIO.setup("USR%d" % i, GPIO.OUT)
 
 while True:
-    for i in range(4):
-        GPIO.output("USR%d" % i, GPIO.HIGH)
-        time.sleep(1)
-    for i in range(4):
+    GPIO.output("USR%d" % i, GPIO.HIGH)
+    time.sleep(0.1) 
+    for n in (1, 2, 4):
         GPIO.output("USR%d" % i, GPIO.LOW)
-        time.sleep(1)
+        time.sleep(0.1)
